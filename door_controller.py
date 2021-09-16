97d8b797c420ac7b9e96e23a4b6d20c7ed135dbb
Phillip Shreves <phillipshreves@gmail.com>
2021-09-15 21:06:30 -0500

    Added second PIR sensor

--- door_controller.py
+++ door_controller.py
@@ -3,27 +3,33 @@ import time
 
 power_supply = gpiozero.DigitalOutputDevice(4)
 actuator_reversal = gpiozero.DigitalOutputDevice(27)
-pir_sensor = gpiozero.MotionSensor(24)
+pir_sensor_one = gpiozero.MotionSensor(23)
+pir_sensor_one = gpiozero.MotionSensor(24)
 
 
 def wait_for_dog():
     print("Motion detected")
     if actuator_reversal.value == 0 and power_supply.value == 1:
         power_supply.off()
-        time.sleep(3)
-        pir_sensor.wait_for_no_motion()
+        pir_sensor_one.wait_for_no_motion()
+        pir_sensor_two.wait_for_no_motion()
+        print("No Motion Detected")
         power_supply.on()
 
 
 def close():
     print("Door closing")
-    pir_sensor.wait_for_no_motion()
+    pir_sensor_one.wait_for_no_motion()
     power_supply.on()
     for wait_time in range(600):
-        if pir_sensor.motion_detected:
+        if pir_sensor_one.motion_detected:
+            wait_for_dog()
+        if pir_sensor_two.motion_detected:
             wait_for_dog()
         time.sleep(0.1)
+        time.sleep(0.1)
     power_supply.off()
+    print("Door closed")
     
 
 
