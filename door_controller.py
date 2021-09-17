fb7c5238c7b8496b2da3812f5b14efd1985cf652
Phillip Shreves <hi@phillips.work>
2021-09-16 21:24:53 -0500

    Changed back to loop for door closing

--- door_controller.py
+++ door_controller.py
@@ -4,33 +4,28 @@ import gpio_assignments
 
 
 def wait_for_dog():
-    print("Motion detected")
-    print(gpio_assignments.actuator_reversal.value)
-    print(gpio_assignments.power_supply.value)
     if gpio_assignments.actuator_reversal.value == 0:
         gpio_assignments.power_supply.off()
         gpio_assignments.pir_sensor_one.wait_for_no_motion()
         gpio_assignments.pir_sensor_two.wait_for_no_motion()
-        print("No Motion Detected")
         gpio_assignments.power_supply.on()
 
 
 def close():
-    print("Door closing")
-    gpio_assignments.pir_sensor_one.wait_for_no_motion()
     gpio_assignments.power_supply.on()
-    time.sleep(60)
+    for time in range(100):
+        gpio_assignments.pir_sensor_one.when_motion = wait_for_dog
+        gpio_assignments.pir_sensor_two.when_motion = wait_for_dog
+        time.sleep(0.1)
     gpio_assignments.power_supply.off()
-    print("Door closed")
     
 
 
 def open():
-    print("Door opening")
     gpio_assignments.actuator_reversal.on()
     gpio_assignments.power_supply.on()
     time.sleep(10)
     gpio_assignments.power_supply.off()
     gpio_assignments.actuator_reversal.off()
-    time.sleep(3)
+    time.sleep(2)
     close()
--- main_reader.py
+++ main_reader.py
@@ -3,10 +3,12 @@ import door_controller
 import gpio_assignments
 from signal import pause
 
+
+#gpio_assignments.pir_sensor_one.when_motion = door_controller.wait_for_dog
+#gpio_assignments.pir_sensor_two.when_motion = door_controller.wait_for_dog
+
 gpio_assignments.button_inside.when_pressed = door_controller.open
 gpio_assignments.button_outside.when_pressed = door_controller.open
-#button_inside.when_held = door_controller.open_forever
-#button_outside.when_held = door_controller.open_forever
-            
+             
 
 pause()
