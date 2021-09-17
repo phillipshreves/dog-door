37cf23510b1acd0b7891476d410d18e3e23d8721
Phillip Shreves <hi@phillips.work>
2021-09-16 21:37:10 -0500

    Debugging

--- door_controller.py
+++ door_controller.py
@@ -13,7 +13,7 @@ def wait_for_dog():
 
 def close():
     gpio_assignments.power_supply.on()
-    for time in range(100):
+    for waiting in range(100):
         gpio_assignments.pir_sensor_one.when_motion = wait_for_dog
         gpio_assignments.pir_sensor_two.when_motion = wait_for_dog
         time.sleep(0.1)
