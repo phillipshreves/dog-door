47b8c8d3c1f9c5c0f292f0854a264103f6242c03
Phillip Shreves <phillipshreves@gmail.com>
2021-10-29 20:49:32 -0500

    Adjusted door timing

--- door_controller.py
+++ door_controller.py
@@ -6,13 +6,13 @@ import gpio_assignments
 def wait_for_dog():
     if gpio_assignments.actuator_reversal.value == 0:
         gpio_assignments.power_supply.off()
-        time.sleep(1)
+        time.sleep(2)
         gpio_assignments.power_supply.on()
 
 
 def close():
     gpio_assignments.power_supply.on()
-    for waiting in range(200):
+    for waiting in range(170):
         if gpio_assignments.pir_sensor.motion_detected:
             wait_for_dog()
         time.sleep(0.2)
@@ -23,7 +23,7 @@ def close():
 def open():
     gpio_assignments.actuator_reversal.on()
     gpio_assignments.power_supply.on()
-    time.sleep(30)
+    time.sleep(27)
     gpio_assignments.power_supply.off()
     gpio_assignments.actuator_reversal.off()
     time.sleep(5)
